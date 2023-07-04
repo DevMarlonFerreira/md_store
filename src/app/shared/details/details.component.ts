f44485806bae5656d9edeb171410071c0a2d1ac7
Marlon <devmarlonferreira@gmail.com>
2023-07-04 15:37:45 -0300

    Update

--- src/app/shared/details/details.component.ts
+++ src/app/shared/details/details.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '../../shared//button/button.component';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './details.component.html',
   styleUrls: ['./details.component.css']
 })
