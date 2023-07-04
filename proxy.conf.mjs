4cfa41ab47bf5811c139acda42aa7e797c6c0444
Marlon <devmarlonferreira@gmail.com>
2023-07-03 22:10:47 -0300

    Update

--- proxy.conf.mjs
+++ proxy.conf.mjs
@@ -2,8 +2,6 @@ export default [
   {
     context: "/api",
     target: "https://mandalorian-store.netlify.app",
-    // target: "https://api.adviceslip.com/advice",
-    // pathRewrite: { "^/api": "" },
     secure: false,
     changeOrigin: true,
     logLevel: "debug",
--- src/app/core/interceptors/api.interceptor.ts
+++ src/app/core/interceptors/api.interceptor.ts
@@ -16,25 +16,10 @@ export class ApiInterceptor implements HttpInterceptor {
   ): Observable<HttpEvent<any>> {
     const apiReq = req.clone({
       // url: `https://mandalorian-store.netlify.app/api${req.url}`,
-      // url: `https://api.adviceslip.com/advice/api`,
       headers: new HttpHeaders({
-        'Content-Type': 'text/plain',
-        // 'Access-Control-Allow-Origin': '*',
-        // 'Access-Control-Allow-Credentials': 'true',
-        // 'Access-Control-Allow-Headers': 'Content-Type',
-        // 'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE',
-        // "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
+        'Content-Type': 'application/json',
       }),
     });
-
-    // const apiReq = req.clone({ headers: req.headers.set('Access-Control-Allow-Origin','https://my-json-server.typicode.com') });
-  //   req = req.clone({
-  //     setHeaders: {
-  //         'Content-Type': 'application/json; charset=utf-8',
-  //         Accept: 'application/json'
-  //     }
-  // });
-
     return next.handle(apiReq);
   }
 }
