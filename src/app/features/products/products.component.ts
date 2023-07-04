6da625aee6f33297f9b75d8beb7b65b37b129b23
Marlon <devmarlonferreira@gmail.com>
2023-07-04 16:55:01 -0300

    Update

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -1,13 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { takeUntil, tap, catchError } from 'rxjs/operators';
+import { Subject, throwError } from 'rxjs';
+
+import { EquipmentService } from '../../core/services/equipment.service';
+import { Equipment } from '../../core/models/equipment.model';
+import { GridComponent } from '../../shared/grid/grid.component';
+import { ContainerComponent } from '../../shared/container/container.component';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, GridComponent, ContainerComponent],
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.css']
+  styleUrls: ['./products.component.css'],
 })
 export class ProductsComponent {
+  equipments: Equipment[] = [];
 
+  equipments$ = inject(EquipmentService)
+    .getAll()
+    .pipe(tap(() => (this.equipmentsLoaded = true)));
+  equipmentsLoaded = false;
+  destroy$ = new Subject<void>();
+
+  constructor(private readonly equipmentService: EquipmentService) {}
+
+  ngOnInit(): void {
+    this.equipmentService.getAll().subscribe((equipments: any) => {
+      this.equipments = equipments;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
